9dcc303730f4c8f04c48d82c2d03e312e7580707
cukiprit <andhikadwikyp@gmail.com>
2023-09-19 23:30:43 +0700

    Final

--- dashboard/main.py
+++ dashboard/main.py
@@ -63,8 +63,8 @@ def create_registered_daily_rentals(df):
     return register_rentals
 
 
-categorical_df = pd.read_csv("./categorical_df.csv")
-numerical_df = pd.read_csv("./numerical_df.csv")
+categorical_df = pd.read_csv("./dashboard/categorical_df.csv")
+numerical_df = pd.read_csv("./dashboard/numerical_df.csv")
 
 monthly_data = numerical_df.groupby("mnth").sum()["cnt"]
 
@@ -328,6 +328,29 @@ with st.container():
     st.subheader("Seasonal Trends")
     st.line_chart(data=monthly_data)
 
+    cat_col_vis = ["season", "mnth"]
+
+    fig, ax = plt.subplots(ncols=1, nrows=2, figsize=(15, 15))
+    i = 0
+
+    for cols in cat_col_vis:
+        sns.barplot(
+            x=categorical_df[cols],
+            y=categorical_df["cnt"],
+            ax=ax[i],
+            edgecolor="#c5c6c7",
+        )
+
+        ax[i].set_xlabel(" ")
+        ax[i].set_ylabel(" ")
+        ax[i].xaxis.set_tick_params(labelsize=14)
+        ax[i].tick_params(left=False, labelleft=False)
+        ax[i].set_ylabel(cols, fontsize=16)
+        ax[i].bar_label(ax[i].containers[0], size="12")
+        i = i + 1
+
+    st.pyplot(plt)
+
     col1, col2 = st.columns(2)
 
     with col1:
@@ -344,14 +367,23 @@ with st.container():
 
     st.caption(
         """
-        The data represents the total number of bike rentals for each month of the year. Here’s a more specific interpretation:
+        here’s a more specific interpretation of the data:
 
-        - The year starts with a moderate number of rentals in January (134,933), which increases slightly in February (151,352).
-        - In March, there’s a significant spike to 2,228,920 rentals. This could be due to warmer weather or other seasonal factors that make biking more popular.
-        - From April to September, the numbers remain relatively high, ranging from 269,094 to 351,194. This period likely represents the peak biking season.
-        - Starting in October, there’s a noticeable decrease in bike rentals, with numbers dropping to 211,036 by December. This could be due to colder weather or other factors that make biking less popular.
+        Seasonal Trends:
 
-        This pattern suggests a strong seasonal trend in bike rentals, with demand peaking in the warmer months and decreasing in the colder months.
+        - Spring: The average number of bike rentals is 2,604.13. This is the lowest among all seasons, possibly due to the transition from colder to warmer weather.
+        - Summer: The average number of bike rentals increases significantly to 4,992.33, likely due to warmer and more favorable weather for outdoor activities like biking.
+        - Fall: The average number of bike rentals is at its peak at 5,644.3. The mild weather during this season might be ideal for biking.
+        - Winter: The average number of bike rentals decreases to 4,728.16, likely due to colder weather.
+
+        Monthly Trends:
+
+        - The year starts with a moderate number of rentals in January (2,176.34), which increases slightly in February (2,655.3).
+        - In March, there’s a significant spike to 3,692.26 rentals. This could be due to warmer weather or other seasonal factors that make biking more popular.
+        - From April to September, the numbers remain relatively high, ranging from 4,484.9 (April) to 5,766.52 (September). This period likely represents the peak biking season.
+        - Starting in October, there’s a noticeable decrease in bike rentals, with numbers dropping to 3,403.81 by December. This could be due to colder weather or other factors that make biking less popular.
+
+        This pattern suggests a strong seasonal trend in bike rentals, with demand peaking in the warmer months and decreasing in the colder months. It also shows that within each season, certain months (like March in Spring, September in Fall) might have higher bike rentals due to specific weather conditions or events.
         """
     )
 
@@ -450,59 +482,163 @@ with st.container():
 
 with st.container():
     st.subheader("Weather Impact")
-    fig = plt.figure(figsize=(10, 5))
+
+    plt.figure(figsize=(10, 5))
     sns.boxplot(x="weathersit", y="cnt", data=numerical_df)
-    st.pyplot(fig)
+    plt.title("Bike Rentals by Weather Situation")
+    plt.xlabel("Weather Situation")
+    plt.ylabel("Total Rentals")
+
+    st.pyplot(plt)
 
     st.caption(
         """
-        This graph is titled “Bike Rentals by Weather Situation”. The x-axis represents the “Weather Situation” with three categories: 1, 2, and 3. The y-axis represents the “Total Rentals” ranging from 0 to 8000.
+        This graph is titled “Bike Rentals by Weather Situation”. The x-axis represents the “Weather Situation” with three categories: 1, 2, and 3. The y-axis represents the “Total Rentals” ranging from 0 to 8000. Weather significantly affects bike rentals in the following ways:
 
-        - Weather situation 1 (represented by the blue bar) had approximately 7500 rentals.
-        - Weather situation 2 (represented by the orange bar) had approximately 5000 rentals.
-        - Weather situation 3 (represented by the green bar) had approximately 2500 rentals.
+        - Clear, Few clouds, Partly cloudy, Partly cloudy (Weather Situation 1):
+            
+            This is the most favorable weather for bike rentals with approximately 7500 rentals. Clear or partly cloudy weather is ideal for outdoor activities like biking.
 
-        Each bar has error bars, indicating the uncertainty in the data. From this graph, it’s clear that the number of bike rentals varies significantly depending on the weather situation. Specifically, weather situation 1 has the most rentals, while weather situation 3 has the least. This suggests that weather has a significant impact on bike rentals
+        - Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist (Weather Situation 2):
+        
+            This weather situation results in a decrease in bike rentals to approximately 5000. The presence of mist or broken clouds might make biking less appealing.
+
+        - Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds (Weather Situation 3):
+        
+            This weather situation has the least favorable conditions for biking with approximately 2500 rentals. Inclement weather like rain, snow, or thunderstorms can discourage people from outdoor activities like biking due to safety concerns and discomfort.
+
+        These trends suggest that favorable weather conditions (clear or partly cloudy) result in higher bike rentals while unfavorable conditions (rain, snow, thunderstorms) result in fewer rentals.
+        """
+    )
+
+with st.container():
+    st.subheader("User Impact")
+
+    numerical_df["casual_percentage"] = numerical_df["casual"] / numerical_df["cnt"]
+    numerical_df["registered_percentage"] = (
+        numerical_df["registered"] / numerical_df["cnt"]
+    )
+
+    plt.figure(figsize=(10, 5))
+    sns.lineplot(data=numerical_df, x="mnth", y="casual_percentage", label="Casual")
+    sns.lineplot(
+        data=numerical_df, x="mnth", y="registered_percentage", label="Registered"
+    )
+    plt.title("Casual vs Registered Users Over Time")
+    plt.xlabel("Month")
+    plt.ylabel("Percentage of Total Rentals")
+    plt.legend()
+
+    st.pyplot(plt)
+
+    st.caption(
+        """
+        Based on the line graph titled “Casual vs Registered Users Over Time”, we can observe the following rental patterns:
+
+        - Casual Users (Orange Line):
+            
+            The percentage of total rentals by casual users is higher than that of registered users for all months. There is a slight dip around the 6th month, after which it increases again. This suggests that casual users might be more influenced by factors such as weather, holidays, or events that occur around this time.
+
+        - Registered Users (Blue Line):
+        
+            The percentage of total rentals by registered users is lower than that of casual users. There is a slight increase around the 6th month, after which it decreases again. This could indicate that registered users have more consistent usage patterns throughout the year, but there might be certain times (like the 6th month) when their usage increases.
+
+        In summary, casual users tend to make up a higher percentage of total rentals and their usage appears to be more variable. On the other hand, registered users have a lower but more consistent percentage of total rentals.
         """
     )
 
 with st.container():
     st.subheader("Conclusion")
-    st.caption(
-        """
-        1. Is there a correlation between weather conditions and bike rentals?
 
-            Here’s a breakdown:
+    with st.expander("How do bike rentals vary across different seasons and months?"):
+        st.caption(
+            """
+            These trends suggest that weather and temperature play a significant role in influencing bike rental patterns. Warmer months and seasons tend to see higher demand for bike rentals, while colder periods see a decrease in demand.
+            """
+        )
 
-            - For all weather conditions, the maximum number of rentals was 3410 and the minimum was 2. This wide range suggests that weather conditions could significantly impact bike rentals.
+    with st.expander("How does weather affect bike rentals?"):
+        st.caption(
+            """
+            - Clear, Few clouds, Partly cloudy, Partly cloudy (Weather Situation 1): This is the most favorable weather for bike rentals with approximately 7500 rentals. Clear or partly cloudy weather is ideal for outdoor activities like biking.
 
-            - For cloudy weather conditions (including mist and broken clouds), the maximum number of rentals was 8362 and the minimum was 605. These figures are higher than the overall maximum and minimum, suggesting that these weather conditions might be more favorable for bike rentals.
+            - Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist (Weather Situation 2): This weather situation results in a decrease in bike rentals to approximately 5000. The presence of mist or broken clouds might make biking less appealing.
 
-            - For clear or partly cloudy weather conditions, the maximum number of rentals was even higher at 8714, while the minimum was 431. This suggests that clear or partly cloudy weather is likely the most favorable condition for bike rentals.
+            - Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds (Weather Situation 3): This weather situation has the least favorable conditions for biking with approximately 2500 rentals. Inclement weather like rain, snow, or thunderstorms can discourage people from outdoor activities like biking due to safety concerns and discomfort.
+            """
+        )
 
-            - For adverse weather conditions like light snow or rain, the maximum number of rentals dropped to 4639 and the minimum was as low as 22. This indicates that such weather conditions are likely to deter people from renting bikes.
+    with st.expander(
+        "What are the differences between casual and registered users in terms of rental patterns?"
+    ):
+        st.caption(
+            """
+            - Casual Users: 
+                
+                The percentage of total rentals by casual users is higher than that of registered users for all months. There is a slight dip around the 6th month, after which it increases again. This suggests that casual users might be more influenced by factors such as weather, holidays, or events that occur around this time.
 
-            In conclusion, there seems to be a strong correlation between weather conditions and bike rentals, with clear or partly cloudy weather being the most favorable for bike rentals, and adverse weather conditions like light snow or rain being the least favorable.
-        """
-    )
-    st.caption(
-        """
-        2. How does the time of year affect the behavior of casual versus registered users?
+            - Registered Users:
+            
+                The percentage of total rentals by registered users is lower than that of casual users. There is a slight increase around the 6th month, after which it decreases again. This could indicate that registered users have more consistent usage patterns throughout the year, but there might be certain times (like the 6th month) when their usage increases.
+            """
+        )
 
-            Based on the data provided, it appears that both the season and weather conditions have a significant impact on bike rentals for both registered and casual users. Here’s a breakdown:
+    with st.expander(
+        "Is there a correlation between weather conditions and bike rentals?"
+    ):
+        st.caption(
+            """
+            there seems to be a strong correlation between weather conditions and bike rentals, with clear or partly cloudy weather being the most favorable for bike rentals, and adverse weather conditions like light snow or rain being the least favorable.
+            """
+        )
 
-            - **For registered users**
-                * In the spring season, the maximum number of rentals was highest under clear or partly cloudy weather conditions (45,315 rentals), while the minimum number of rentals was lowest under light snow or rain conditions (432 rentals).
-                * In the summer season, the maximum number of rentals was highest under clear or partly cloudy weather conditions (6,456 rentals), while the minimum number of rentals was lowest under light snow or rain conditions (674 rentals).
-                * In the fall season, the maximum number of rentals was highest under clear or partly cloudy weather conditions (6,971 rentals), while the minimum number of rentals was lowest under light snow or rain conditions (1,689 rentals).
-                * In the winter season, the maximum number of rentals was highest under clear or partly cloudy weather conditions (6,946 rentals), while the minimum number of rentals was lowest under light snow or rain conditions (20 rentals).
+    with st.expander(
+        "How does the time of year affect the behavior of casual versus registered users?"
+    ):
+        st.caption(
+            """
+            It appears that both registered and casual users prefer to rent bikes under clear or partly cloudy weather conditions across all seasons. However, registered users seem to be more tolerant of adverse weather conditions compared to casual users. This information could be valuable for predicting bike rental patterns and informing marketing strategies. For instance, additional promotions could be planned for times when the weather is expected to be clear or partly cloudy, which might boost rentals among casual users. Conversely, strategies could be developed to encourage bike rentals among casual users during adverse weather conditions.
+            """
+        )
 
-            - **For casual users**
-                * In the spring season, the maximum number of rentals was highest under misty or cloudy weather conditions (3,155 rentals), while the minimum number of rentals was lowest under light snow or rain conditions (9 rentals).
-                * In the summer season, the maximum number of rentals was highest under clear or partly cloudy weather conditions (3,410 rentals), while the minimum number of rentals was lowest under light snow or rain conditions (120 rentals).
-                * In the fall season, the maximum number of rentals was highest under clear or partly cloudy weather conditions (3,160 rentals), while the minimum number of rentals was lowest under light snow or rain conditions (118 rentals).
-                * In the winter season, the maximum number of rentals was highest under clear or partly cloudy weather conditions (3,031 rentals), while the minimum number of rentals was lowest under light snow or rain conditions (2 rentals).
+    with st.expander(
+        "How can we optimize bike availability according to seasonal trends?"
+    ):
+        st.caption(
+            """
+            - Increase Availability During Peak Seasons:
+            
+                The data shows that bike rentals peak during the summer and fall seasons. Therefore, it would be beneficial to increase bike availability during these months to meet the high demand.
+            
+            - Maintenance and Repair During Off-Peak Seasons:
+                
+                The winter and spring seasons show a lower demand for bike rentals. This would be a good time to schedule regular maintenance and repairs to ensure that the bikes are in optimal condition for the peak season.
 
-            In conclusion, it appears that both registered and casual users prefer to rent bikes under clear or partly cloudy weather conditions across all seasons. However, registered users seem to be more tolerant of adverse weather conditions compared to casual users. This information could be valuable for predicting bike rental patterns and informing marketing strategies. For instance, additional promotions could be planned for times when the weather is expected to be clear or partly cloudy, which might boost rentals among casual users. Conversely, strategies could be developed to encourage bike rentals among casual users during adverse weather conditions.
-        """
-    )
+            - Promotional Activities:
+                
+                To encourage bike rentals during off-peak seasons or months with lower demand, promotional activities such as discounts or loyalty programs could be introduced.
+
+            - Alternative Usage:
+                
+                During colder months when demand is low, consider alternative uses for the bikes. For example, they could be rented out for longer periods or used in partnership with local tour operators for guided tours.
+
+            - Dynamic Pricing:
+                
+                Implement a dynamic pricing model where prices are lower during off-peak seasons to encourage usage, and higher during peak seasons when demand is high.
+
+            - Weather-Proof Bikes:
+                
+                Consider investing in weather-proof bikes or providing additional equipment like rain covers or warmer gear during colder months to attract customers.
+            """
+        )
+
+    with st.expander("Can we predict bike rental demand based on weather conditions?"):
+        st.caption(
+            """
+            Yes, it is possible to predict bike rental demand based on weather conditions using machine learning models. Weather conditions such as temperature, humidity, wind speed, and weather situation (clear, cloudy, rainy, etc.) can significantly influence the demand for bike rentals.
+
+            Based on the graph you provided, we can see clear trends in bike rentals across different weather situations. For instance, Weather Situation 1 (Clear, Few clouds, Partly cloudy) has the highest number of rentals, followed by Weather Situation 2 (Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds), and then Weather Situation 3 (Light Snow, Light Rain + Thunderstorm + Scattered clouds). This suggests that clear or partly cloudy weather is most favorable for bike rentals, while inclement weather like rain or snow reduces the demand.
+
+            These trends can be used to train a machine learning model to predict bike rental demand based on weather conditions. The model could take in weather data as input and output the predicted number of bike rentals. This could be particularly useful for planning and resource allocation in bike rental services.
+            """
+        )
